a7e2e3a14ae63c7cdb9fdbfaff0125235008f991
stanislavd <stanislav.dmitruk@neugelb.com>
2020-12-08 02:48:52 +0100

    Added AlfaBankDeposits and AlfaBankInsurance tests

---
+++ src/test/java/tests/AlfaBankTest.java
@@ -0,0 +1,29 @@
+package tests;
+
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selenide.*;
+
+public class AlfaBankTest {
+    String DepositsUrl = "make-money/deposits/";
+    @Test
+    void AlfaBankDeposits() {
+        open("https://alfabank.ru");
+        $("#alfa").$(byText("Вклады")).click();
+        $("[data-widget-name='Heading']").shouldHave(text("Накопительные продукты"));
+        $("#filter").$(byText("Депозиты")).parent().click();
+        $("#alfa").$(byText("Архивные счета и депозиты")).parent().click();
+        $("#filter").$(byText("Депозиты")).parent().click();
+        $$("[data-widget-name='CatalogCard']").shouldHaveSize(5);
+    }
+
+    @Test
+    void AlfaBankInsurance() {
+        open("https://alfabank.ru/");
+        $("#alfa").$(byText("Вклады")).click();
+        $("#benefit").$("button").parent().click();
+        $("body").shouldHave(text("Страхование вкладов"));
+    }
+}
